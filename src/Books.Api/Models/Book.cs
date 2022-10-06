441375c8a9b6f39e213a9f22e20530119d113fa7
cadamsmith <cadamsmith.dev@gmail.com>
2022-10-06 03:19:35 -0500

    add Book and CreateBookModel models

---
+++ src/Books.Api/Models/Book.cs
@@ -0,0 +1,13 @@
+
+namespace Books.Api.Models;
+
+public class Book
+{
+    public Guid Id { get; set; }
+
+    public string Author { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+}
