4806f95d86ac6a3eef08f92f1412b598b5523cad
cadamsmith <cadamsmith.dev@gmail.com>
2022-10-06 04:23:12 -0500

    configure dbcontext in middleware

--- src/Books.Api/Program.cs
+++ src/Books.Api/Program.cs
@@ -1,14 +1,24 @@
 
 using Microsoft.EntityFrameworkCore;
 
+using Books.Api.Contexts;
 using Books.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// connect to MariaDB database
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+var serverVersion = ServerVersion.AutoDetect(connectionString);
+
+builder.Services.AddDbContext<BooksContext>(
+    options => options.UseMySql(connectionString, serverVersion)
+);
+
 builder.Services.AddControllers();
 
+// add data access classes to middleware
 builder.Services.AddScoped<IBooksRepository, BooksRepository>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
