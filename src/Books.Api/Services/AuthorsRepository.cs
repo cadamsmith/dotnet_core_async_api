08aff6f53fad41c91c3a416ef87d6e4de8d80537
cadamsmith <cadamsmith.dev@gmail.com>
2022-10-06 11:15:57 -0500

    document the repository classes

--- src/Books.Api/Services/AuthorsRepository.cs
+++ src/Books.Api/Services/AuthorsRepository.cs
@@ -6,6 +6,9 @@ using Books.Api.Entities;
 
 namespace Books.Api.Services;
 
+/// <summary>
+/// Handles data access for the Author entity
+/// </summary>
 public class AuthorsRepository : IAuthorsRepository
 {
     private readonly BooksContext _context;
@@ -17,6 +20,11 @@ public class AuthorsRepository : IAuthorsRepository
         _logger = logger ?? throw new ArgumentNullException(nameof(context));
     }
 
+    /// <summary>
+    /// Retrieves an author from context
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public Author? GetAuthor(Guid id)
     {
         if (id == Guid.Empty)
@@ -27,6 +35,11 @@ public class AuthorsRepository : IAuthorsRepository
         return _context.Authors.FirstOrDefault(a => a.Id == id);
     }
 
+    /// <summary>
+    /// Retrieves an author from context asynchronously
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public async Task<Author?> GetAuthorAsync(Guid id)
     {
         if (id == Guid.Empty)
@@ -37,6 +50,14 @@ public class AuthorsRepository : IAuthorsRepository
         return await _context.Authors.FirstOrDefaultAsync(a => a.Id == id);
     }
 
+    /// <summary>
+    /// Adds an author to the context
+    /// </summary>
+    /// <param name="author"></param>
+    /// <remarks>
+    /// this method does not need to be async, because the <c>_context.Add</c>
+    /// operation is not io-bound
+    /// </remarks>
     public void AddAuthor(Author author)
     {
         if (author is null)
@@ -47,6 +68,10 @@ public class AuthorsRepository : IAuthorsRepository
         _context.Add(author);
     }
 
+    /// <summary>
+    /// Saves context, persisting changes
+    /// </summary>
+    /// <returns></returns>
     public bool SaveChanges()
     {
         return (_context.SaveChanges() > 0);
@@ -57,11 +82,16 @@ public class AuthorsRepository : IAuthorsRepository
         return (await _context.SaveChangesAsync() > 0);
     }
 
+    /// <summary>
+    /// saves context asynchronously, persisting changes
+    /// </summary>
+    /// <returns></returns>
     public IEnumerable<Author> GetAuthors()
     {
         return _context.Authors.AsEnumerable();
     }
 
+    
     public IAsyncEnumerable<Author> GetAuthorsAsync()
     {
         return _context.Authors.AsAsyncEnumerable();
--- src/Books.Api/Services/BooksRepository.cs
+++ src/Books.Api/Services/BooksRepository.cs
@@ -6,6 +6,9 @@ using Books.Api.Entities;
 
 namespace Books.Api.Services;
 
+/// <summary>
+/// Handles data access for the Book entity
+/// </summary>
 public class BooksRepository : IBooksRepository
 {
     private readonly BooksContext _context;
@@ -17,6 +20,11 @@ public class BooksRepository : IBooksRepository
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
+    /// <summary>
+    /// Retrieves a book from context
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public Book? GetBook(Guid id)
     {
         if (id == Guid.Empty)
@@ -28,6 +36,11 @@ public class BooksRepository : IBooksRepository
             .FirstOrDefault(b => b.Id == id);
     }
 
+    /// <summary>
+    /// Retrieves a book from context asynchronously
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public async Task<Book?> GetBookAsync(Guid id)
     {
         if (id == Guid.Empty)
@@ -39,6 +52,15 @@ public class BooksRepository : IBooksRepository
             .FirstOrDefaultAsync(b => b.Id == id);
     }
 
+
+    /// <summary>
+    /// adds a book to the context
+    /// </summary>
+    /// <param name="book"></param>
+    /// <remarks>
+    /// this method does not need to be async, because the <c>_context.Add</c>
+    /// operation is not io-bound
+    /// </remarks>
     public void AddBook(Book book)
     {
         if (book is null)
@@ -49,21 +71,37 @@ public class BooksRepository : IBooksRepository
         _context.Add(book);
     }
 
+    /// <summary>
+    /// saves context, persisting changes
+    /// </summary>
+    /// <returns></returns>
     public bool SaveChanges()
     {
         return (_context.SaveChanges() > 0);
     }
 
+    /// <summary>
+    /// saves context asynchronously, persisting changes
+    /// </summary>
+    /// <returns></returns>
     public async Task<bool> SaveChangesAsync()
     {
         return (await _context.SaveChangesAsync() > 0);
     }
 
+    /// <summary>
+    /// retrieves all books in context
+    /// </summary>
+    /// <returns></returns>
     public IEnumerable<Book> GetBooks()
     {
         return _context.Books.AsEnumerable();
     }
 
+    /// <summary>
+    /// retrieves all books in context as an async enumerable
+    /// </summary>
+    /// <returns></returns>
     public IAsyncEnumerable<Book> GetBooksAsync()
     {
         return _context.Books.AsAsyncEnumerable();
